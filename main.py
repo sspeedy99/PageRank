aa8a5de38e0577e7a0cde03310a96158046e7512
Shivam Ralli <36051274+sralli@users.noreply.github.com>
2018-11-26 00:54:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/bin/env python3
+
+import numpy as np
+from scipy.sparse.linalg import eigs
+import os
+import time
+import glob
+import Files
+
+def main():    
+    # Figure out how many files there are to read, construct matrix with that size
+    count = int(os.popen("ls Pages/| wc -l").read().strip())
+    print(count)
+    matrix = [x[:] for x in [[0]*count]*count]
+
+    # Parse the text files and populate matrix with data from Parser
+    parserOutput = os.popen("./parser.sh").read().split()
+    i = 0
+    while i < len(parserOutput):
+        matrix[int(parserOutput[i])][int(parserOutput[i+1])] = float(parserOutput[i+2])
+        i += 3
+        
+    # Output Data while calculating the Eigenvectors
+    print("Constructed Matrix:\n" + str(np.matrix(matrix)) + "\n\n")
+    vals, vecs = eigs(np.array(matrix), k=1)
+    print("Eigenvalues:\n" + str(np.matrix(vals)) + "\n\n")
+    print("Eigenvectors:\n"+ str(np.matrix(vecs)) + "\n\n")
+
+    # Discard Complex variables
+    eigenvector = []
+    for vec in vecs:
+        eigenvector.append(float(vec[0]))
+
+    # Print Search Results
+    print("\nSearch Results:")
+    for page in np.argsort(eigenvector)[::-1]:
+        print("Page " + str(page+1))
+
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))
+
+files = glob.glob('Pages/*.html')
+for f in files:
+    os.remove(f)
